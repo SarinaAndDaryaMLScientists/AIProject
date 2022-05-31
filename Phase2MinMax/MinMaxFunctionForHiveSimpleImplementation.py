0f1b195010fdbbfda5bd6b57a5fca313006909c0
darya <darya2000a0@gmail.com>
2022-05-31 10:08:55 +0430

    Initial commit

--- Phase2MinMax/MinMaxFunctionForHiveSimpleImplementation.py
+++ Phase2MinMax/MinMaxFunctionForHiveSimpleImplementation.py
@@ -5,7 +5,8 @@ from AnotherMinMaxAlgorithm.MinMaxAlgorithm import MinMaxAlgorithm
 row = 4
 col = 4
 
-#tamame harakate
+
+# tamame harakate
 class BoardThings():
     def getGameResults(self, board1):
         if findAIScore(board) == 6:
@@ -16,7 +17,7 @@ class BoardThings():
         return 1
 
     def getMoves(self, board, maximizingColor):
-        #tamame mohre hayi k hastan tamame harkatashoon.
+        # tamame mohre hayi k hastan tamame harkatashoon.
         selections = []
         moves = []
         for i in range(0, row):
@@ -26,13 +27,12 @@ class BoardThings():
                         selections.append(('select', i, j))
         if maximizingColor == humanPlayer.color:
             for u in humanPlayer.usedPieces:
-                if(isMoveAblePiece(u)):#todo
+                if (isMoveAblePiece(u)):  # todo
                     moves.append(get_available_moves(u))
         else:
             for u in AI.usedPieces:
-                if(isMoveAblePieve(u)):
-                    moves.append(get_available_moves(u))   #todo
-
+                if (isMoveAblePieve(u)):
+                    moves.append(get_available_moves(u))  # todo
 
 
 class BeeKind(enum.Enum):
@@ -61,6 +61,7 @@ def neighbor_points(x, y):
 
     return res
 
+
 def findHumanScore(board1):
     counter = 0
     for i in range(0, row):
@@ -71,6 +72,7 @@ def findHumanScore(board1):
                         if board1[a][b] != 0 and board1[a][b] > 10:
                             counter = counter + 1
                 return counter
+
 
 # 11 0 0
 def findAIScore(board1):
@@ -187,7 +189,6 @@ class InteractiveHumanPlayer(object):
 
         }
 
-
     def makeChoice(self, board):
         kindPlay = input("please enter your move type: m for move p for placement")
         if kindPlay == 'p':
@@ -205,9 +206,40 @@ AI = AIAgent()
 colors = np.zeros((row, col))
 board = np.zeros((row, col))
 turn = True
+
+
+def getIndex(board, param):
+    for i in range(row):
+        for j in range(col):
+            if board[i][j] == param:
+                return i, j
+    return -1, -1
+
+
+def countValidNeighbors(board, x, y):
+    counter = 0
+    for i, j in neighbor_points(x, y):
+        if 0 <= i <= row and 0 <= j <= col:
+            if board[i][j] != 0:
+                counter = counter + 1
+    return counter
+
+
+def getGameResult(board):
+    x, y = getIndex(board, 11)
+    x1, y1 = getIndex(board, 1)
+    if x1 == -1 or x == -1:
+        return 0  # har 2 taraf malake hashoono nazashtan.
+    if countValidNeighbors(board, x, y) == 6:
+        return 1
+    elif countValidNeighbors(board, x1, y1) == 6:
+        return -1;
+    return 0
+
+
 if __name__ == '__main__':
     while True:
-        #1 = WE WON -1 AI 0 HANOOZ TAMOOM NASHODE -3 MOSAVI -5Hameja por shode vli dor malake ha khalie.
+        # 1 = WE WON -1 AI 0 HANOOZ TAMOOM NASHODE -3 MOSAVI -5Hameja por shode vli dor malake ha khalie.
         gameFinished = getGameResult(board)
         if gameFinished != 0:
             print("game finished")
